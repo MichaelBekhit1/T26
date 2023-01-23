e6c2db0cd153b8400963f9d136de3bfe839f12ee
[Michael Bekhit] <[michael.bekhit@gmail.com]>
2023-01-23 17:36:17 +0000

    Register user credentials

--- task_manager.py
+++ task_manager.py
@@ -3,8 +3,17 @@ import mysql.connector
 user_database = mysql.connector.connect(
     host = "localhost",
     user = "code_reviewer",
-    password = "code123"
-)
+    password = "code123",
+    database = "26_capstone_iii"
+    )
 
-print(user_database)
+# code_reviewer has DB privileges only
+# There is no sql server attached to course. The DB is on my local server.
 
+cursor = user_database.cursor()
+
+def login(username, password):
+    pass
+
+username_list = cursor.execute("SELECT username FROM user_credentials")
+print(username_list)
