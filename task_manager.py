46d3824e5606be063e1efb71d4226fc0687d5d71
[Michael Bekhit] <[michael.bekhit@gmail.com]>
2023-01-24 18:11:33 +0000

    fixed validation issues

--- task_manager.py
+++ task_manager.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import string
+import hashlib
 
 # Connects program to database with code_reviewer connection. This connection is insecure.
 
@@ -13,19 +14,9 @@ user_database = mysql.connector.connect(
 # code_reviewer has DB privileges only
 # There is no sql server attached to course. The DB is on my local server and does not allow remote access so I will be marked on syntax.
 
-cursor = user_database.cursor(buffered = True)
+cursor = user_database.cursor(buffered = True) # buffered = True was a fix for a problem. Note: Figure out what is going on here.
 
 
-def register_user():
-    user = input("Please enter the username of the user that you wish to register: ")
-    passw = input("Please enter the password for the user that you wish to register: ")
-    confirm_password = input("Please reenter the password for the user that you wish to register: ")
-    if passw == confirm_password:
-        cursor.execute(f"""
-        INSERT INTO user_credentials (username, password)
-        VALUES ('{user}', '{passw}');
-        """)
-        user_database.commit()
 
 def valid_username():
 
@@ -34,12 +25,14 @@ def valid_username():
     minimum_length = 4
 
     # checks username is {minimum length} or more characters long and is not already taken. If input is invalid, input is requested again. {number of attempts} are allowed.
+    
 
     for _ in range(number_of_attempts):
 
         username = input("Please enter your username: ")
-        cursor.execute(f"SELECT username FROM 26_capstone_iii.user_credentials WHERE username = '{username}';")
+        cursor.execute(f"SELECT username FROM 26_capstone_iii.user_credentials WHERE username = '{(hashlib.sha1(username.encode())).hexdigest()}';")
         name = cursor.fetchone()
+
         if mistake_count >= (number_of_attempts -1):
             print("You have had too many unsuccessful attempts.")
             exit()
@@ -61,12 +54,14 @@ def valid_password():
     punctuation_count = 0
     minimum_punctuation_count = 1
 
-    # checks password is  {minimum length} and has {minimum punctuation count} punctuation chars. {number of attempts} attempts are allowed.
-
+    
     for _ in range(number_of_attempts):
 
-        password = input("Please enter your password: ")
+    # checks password is  {minimum length} and has {minimum punctuation count} punctuation chars. {number of attempts} attempts are allowed.
 
+        password = input("Please enter your password: ")
+        confirm_password = input("Please reenter the password for the user that you wish to register: ")
+        
         for punctuation_char in range(len(string.punctuation)):
             for char in range(len(password)):
                 if password[char] == string.punctuation[punctuation_char]:
@@ -75,8 +70,14 @@ def valid_password():
         if mistake_count >= (number_of_attempts -1):
                 print("You have had too many unsuccessful attempts.")
                 exit()
+
+        elif password != confirm_password:
+            print("Your passwords do not match. Please try again.")
+            mistake_count += 1
+
+        
         elif len(password) < minimum_length:
-                print(f"Your username must be at least {minimum_length} characters long. Please try again")
+                print(f"Your password must be at least {minimum_length} characters long. Please try again")
                 mistake_count += 1
 
         elif punctuation_count >= minimum_punctuation_count:
@@ -84,10 +85,28 @@ def valid_password():
 
         else:
             return password
+
+def register_user(valid_username, valid_password):
+    
+    # Injection attack vulnerability dealt with by hashing the username
+
+            cursor.execute(f"""
+            INSERT INTO user_credentials (username, password)
+            VALUES ('{(hashlib.sha1(valid_username.encode())).hexdigest()}', '{(hashlib.sha1(valid_password.encode())).hexdigest()}');
+            """)
+            user_database.commit()
+            print("The user has been registered successfully.")
+            
+        
     
 
 def validate_login(username_input, password_input):
-    cursor.execute(f"SELECT username FROM 26_capstone_iii.user_credentials WHERE (username = '{username_input}' AND password = '{password_input}');")
+
+    # Filter the table for the given username and password and return the username if they are present. If they are not present the login is not valid
+
+    cursor.execute(f"""
+    SELECT username FROM 26_capstone_iii.user_credentials 
+    WHERE (username = '{(hashlib.sha1(username_input.encode())).hexdigest()}' AND password = '{(hashlib.sha1(password_input.encode())).hexdigest()}');""")
     value = cursor.fetchone()
     if value != None:
         return True
@@ -98,8 +117,9 @@ def validate_login(username_input, password_input):
 
 menu = ""
 
-while validate_login(valid_username(), valid_password()) == True and menu != "e":
+while validate_login(valid_username(), valid_password()):
     pass
+
 
 
 
